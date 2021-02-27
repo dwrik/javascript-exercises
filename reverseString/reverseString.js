c2f06e5a6abb7a88deada469cc30b7b15ea62615
Wrik Das <wrikdas22@gmail.com>
2021-02-27 15:42:11 +0530

    Completed reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let reverse = '';
+    chars = string.split('');
+    chars.forEach((char) => reverse = char + reverse);
+
+    return reverse;
 }
 
 module.exports = reverseString
