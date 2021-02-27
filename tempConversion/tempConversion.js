9e750384e0d8e9465ffc935291c021682e9d4e67
Wrik Das <wrikdas22@gmail.com>
2021-02-27 16:49:09 +0530

    Completed tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+// toFixed returns a string
+// so unary + is used to convert to Number
 
+const ftoc = function(ftemp) {
+  return +((ftemp - 32) * 5 / 9).toFixed(1);
 }
 
-const ctof = function() {
 
+const ctof = function(ctemp) {
+  return +(ctemp * 9 / 5 + 32).toFixed(1);
 }
 
 module.exports = {
