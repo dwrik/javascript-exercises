d39d7c280d566bf5cc4479df83c8b20c049e3c6d
Wrik Das <wrikdas22@gmail.com>
2021-02-27 15:57:10 +0530

    Completed removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
 
+    elementsToRemove.forEach(removeElement);
+
+    function removeElement(num) {
+
+        let index = array.indexOf(num);
+
+        if (index >= 0) {
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 }
 
 module.exports = removeFromArray
