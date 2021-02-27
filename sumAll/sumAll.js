40d8aad31af9e51f13bc6ed7f6a9ee7bc832cc6b
Wrik Das <wrikdas22@gmail.com>
2021-02-27 16:17:04 +0530

    Completed sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    start = Math.min(num1, num2);
+    end   = Math.max(num1, num2);
+
+    let sumFromOneToStart = start * (start + 1) / 2;
+    let sumFromOneToEnd   = end   * (end   + 1) / 2;
+
+    return sumFromOneToEnd - sumFromOneToStart + start;
 }
 
 module.exports = sumAll
