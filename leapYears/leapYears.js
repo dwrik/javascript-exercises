c0c7c81d8f03d7a9e63339819e62054579590c2b
Wrik Das <wrikdas22@gmail.com>
2021-02-27 16:32:20 +0530

    Completed leapYears exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let isLeap = false;
+
+    if (year % 100 === 0 && year % 400 === 0) {
+        isLeap = true;
+    }
+
+    if (year % 4 === 0 && year % 100 !== 0) {
+        isLeap = true;
+    }
+
+    return isLeap;
 }
 
 module.exports = leapYears
