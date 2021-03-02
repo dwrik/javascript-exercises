bc7fe34e10653c56d06230e1b3adbcadb4eb0996
Wrik Das <wrikdas22@gmail.com>
2021-03-02 09:18:12 +0530

    Completed palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    // ignores punctuation using regex
+    // and only considers alphnumeric characters
+    string = string.match(/[a-zA-Z0-9]/g).join('');
+    reverse = string.split('').reverse().join('');
+    // ignores case
+    return string.toLowerCase() === reverse.toLowerCase();
 }
 
 module.exports = palindromes
