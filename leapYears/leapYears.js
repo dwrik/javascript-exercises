21a8a2920c6429785b854a01821b4b9fdbc9681c
Wrik Das <wrikdas22@gmail.com>
2021-03-02 08:00:01 +0530

    Updated the solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,16 +1,5 @@
 const leapYears = function(year) {
-
-    let isLeap = false;
-
-    if (year % 100 === 0 && year % 400 === 0) {
-        isLeap = true;
-    }
-
-    if (year % 4 === 0 && year % 100 !== 0) {
-        isLeap = true;
-    }
-
-    return isLeap;
+  return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
 }
 
 module.exports = leapYears
