b316da956d458c6bd81d28d97a616871c3eeadcd
Wrik Das <wrikdas22@gmail.com>
2021-03-02 08:44:47 +0530

    Completed calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,29 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((sum, curr) => sum += curr, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((prod, curr) => prod *= curr, 1);
 }
 
-function power() {
-	
+function power(a, b) {
+	return a ** b;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let fact = 1;
+	for (let i=num; i>=1; i--) {
+		fact *= i;
+	}
+	return fact;
 }
 
 module.exports = {
