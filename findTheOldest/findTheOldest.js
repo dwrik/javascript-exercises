e238967106d17f2ca6f827a274dfb1d57a3e3df2
Wrik Das <wrikdas22@gmail.com>
2021-03-02 10:16:55 +0530

    Completed findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        current.yearOfDeath = current.yearOfDeath ?? new Date().getFullYear();
+        return (oldest.yearOfDeath - oldest.yearOfBirth 
+            > current.yearOfDeath - current.yearOfBirth)? oldest : current;
+    }, {
+        yearOfBirth: 0,
+        yearOfDeath: 0,
+    });
 }
 
 module.exports = findTheOldest
