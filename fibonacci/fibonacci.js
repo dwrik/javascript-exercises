9abb5f0be77359bc1efabaa53a7a36c64692e3ee
Wrik Das <wrikdas22@gmail.com>
2021-03-02 09:47:20 +0530

    Completed fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof(num) == 'string') num = parseInt(num);
+    if (num < 0) return 'OOPS';
+    if (num < 2) return num;
+    let a = 0;
+    let b = 1;
+    for (let i=2; i<=num; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 }
 
 module.exports = fibonacci
