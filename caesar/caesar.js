b0de9d76ab57c33eec3aed4128c80c62ba5ca28a
Wrik Das <wrikdas22@gmail.com>
2021-03-02 11:43:51 +0530

    Completed ceasar exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,35 @@
-const caesar = function() {
+const caesar = function(string, shift) {
 
+    let encrypted = '';
+    alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    string.split('').forEach((char) => {
+
+        let shiftedChar = '';
+        if (char.match(/[a-zA-Z]/g)) {
+
+            let isUpperCase = false;
+            let charCode = char.charCodeAt(0);
+
+            if (charCode >= 65 && charCode <= 90) {
+                isUpperCase = true;
+            }
+
+            let base = (isUpperCase)? 65 : 97;
+            let index = charCode - base;
+            let shiftedIndex = index + shift;
+
+            shiftedIndex = (shiftedIndex < 0)? (alpha.length + shiftedIndex) : (shiftedIndex % alpha.length);
+            shiftedChar = (isUpperCase)? alpha[shiftedIndex].toUpperCase() : alpha[shiftedIndex];
+
+        } else {
+            shiftedChar = char;
+        }
+
+        encrypted += shiftedChar;
+    });
+
+    return encrypted;
 }
 
 module.exports = caesar
