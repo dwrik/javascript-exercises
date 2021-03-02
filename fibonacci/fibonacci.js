ad0f14cdbc1484d7c60f17b19efc3b377ee37dc1
Wrik Das <wrikdas22@gmail.com>
2021-03-02 09:53:22 +0530

    Updated solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,15 +1,14 @@
 const fibonacci = function(num) {
-    if (typeof(num) == 'string') num = parseInt(num);
     if (num < 0) return 'OOPS';
-    if (num < 2) return num;
+    if (num == 0) return 0;
     let a = 0;
     let b = 1;
-    for (let i=2; i<=num; i++) {
-        c = a + b;
+    for (let i=1; i<num; i++) {
+        const c = a + b;
         a = b;
         b = c;
     }
-    return c;
+    return b;
 }
 
 module.exports = fibonacci
