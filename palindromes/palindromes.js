80873b2e54ba61dc17a0c40a99f65d04fc2a3540
Wrik Das <wrikdas22@gmail.com>
2021-03-02 09:23:20 +0530

    Updated solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,10 +1,9 @@
 const palindromes = function(string) {
-    // ignores punctuation using regex
-    // and only considers alphnumeric characters
-    string = string.match(/[a-zA-Z0-9]/g).join('');
-    reverse = string.split('').reverse().join('');
-    // ignores case
-    return string.toLowerCase() === reverse.toLowerCase();
+    processedString = string.match(/[a-zA-Z]/g).join('').toLowerCase();
+    return (processedString
+        .split('')
+        .reverse()
+        .join('') === processedString);
 }
 
 module.exports = palindromes
